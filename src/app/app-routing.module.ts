156b97d2499163be9e838bbad4c3f340e6b6bf8b
Akshaya <akshayarh@gmail.com>
2021-04-04 17:30:11 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent} from './home/home.component';
+import { BlogComponent} from './blog/blog.component';
+import { GalleryComponent} from './gallery/gallery.component';
+import { HelpComponent} from './help/help.component';
+import { NotfoundComponent} from './notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'blog',
+    component:BlogComponent
+  },
+  {
+    path:'gallery',
+    component:GalleryComponent
+  },
+  {
+    path:'help',
+    component:HelpComponent
+  },
+  {
+    path:'**',
+    component:NotfoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { HelpComponent } from './help/help.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogComponent,
+    GalleryComponent,
+    FooterComponent,
+    NotfoundComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule,
